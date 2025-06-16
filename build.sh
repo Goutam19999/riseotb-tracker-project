02c1b749836e44cec0bcce0e259a5eaaedddc054
Goutam Manna <goutammanna97@gmail.com>
2025-06-16 21:15:21 +0545

    Add build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Run migrations and collect static files
 python manage.py migrate
 python manage.py collectstatic --noinput
